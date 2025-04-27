b8291acda0d2efc981693d32722b1e35a12faae2
dessert99 <lsjh1234@naver.com>
2025-04-27 22:22:22 +0900

    contact 추가

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
 function Contact() {
-  return <div></div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            <div>
+              <a href="https://github.com/Dessert99?tab=repositories" target="_blank" rel="noreferrer">
+                KAKAO : Webstroyboy
+              </a>
+            </div>
+            <div>
+              <a href="https://github.com/Dessert99?tab=repositories" target="_blank" rel="noreferrer">
+                mail : WEBSTORYBOY@NAVER.COM
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
